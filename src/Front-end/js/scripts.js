89d9d7b7a0a2a4a81047d10d97f2a0466591c3a0
Gustavo Ferreira <65983794+gustavofdeoliveira@users.noreply.github.com>
2022-11-12 16:05:00 -0300

    Dashboard Finalizado

---
+++ src/Front-end/js/scripts.js
@@ -0,0 +1,63 @@
+window.onload = function () {
+    var duration = 2; // Converter para segundos
+    debugger
+    document.getElementById("body-pd").style.display = "none";
+    document.getElementById("body-pd").insertAdjacentHTML("beforebegin", '<div class="container-background" id="loader"><div class="loader"><div></div>');
+
+    startTimer(duration); // iniciando o timer
+
+}
+
+function startTimer(duration,) {
+    var timer = duration, seconds;
+    var setIntervalo = setInterval(function () {
+        seconds = parseInt(timer % 60, 10);
+        if (--timer <= 0) {
+            clearInterval(setIntervalo);
+            document.getElementById("loader").remove();
+            document.getElementById("body-pd").style.display = "flex";
+
+        }
+    }, 1000);
+}
+
+
+
+document.addEventListener("DOMContentLoaded", function (event) {
+
+    const showNavbar = (toggleId, navId, bodyId, headerId) => {
+        const toggle = document.getElementById(toggleId),
+            nav = document.getElementById(navId),
+            bodypd = document.getElementById(bodyId),
+            headerpd = document.getElementById(headerId)
+
+        // Validate that all variables exist
+        if (toggle && nav && bodypd && headerpd) {
+            toggle.addEventListener('click', () => {
+                // show navbar
+                nav.classList.toggle('show')
+                // change icon
+                toggle.classList.toggle('bx-x')
+                // add padding to body
+                bodypd.classList.toggle('body-pd')
+                // add padding to header
+                headerpd.classList.toggle('body-pd')
+            })
+        }
+    }
+
+    showNavbar('header-toggle', 'nav-bar', 'body-pd', 'header')
+
+    /*===== LINK ACTIVE =====*/
+    const linkColor = document.querySelectorAll('.nav_link')
+
+    function colorLink() {
+        if (linkColor) {
+            linkColor.forEach(l => l.classList.remove('active'))
+            this.classList.add('active')
+        }
+    }
+    linkColor.forEach(l => l.addEventListener('click', colorLink))
+
+    // Your code to run since DOM is loaded and ready
+});
