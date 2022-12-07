55eeb2d36903e04bc84e25291140788a97fbe15e
Gustavo Ferreira <65983794+gustavofdeoliveira@users.noreply.github.com>
2022-12-07 15:12:32 -0300

    Código dos ESP32

--- src/Back-end/ESP32/código-funcionando/roteador.cpp
+++ src/Back-end/ESP32/código-funcionando/roteador.cpp
@@ -1,71 +1,60 @@
+#include <WiFiManager.h>
 #include <Arduino.h>
 #include <WiFi.h>
 #include <iostream>
 #include <WiFiClient.h>
-
 #include <ArduinoJson.h>
 #include <HTTPClient.h>
 
-#define SEVIDOR_ENVIO "http://192.168.183.104:3001/device/move"
-
-const char* SSIDS = "Cel's Galaxy A53 5G";
-const char* PWD = "qhvz8247";
-HTTPClient http; 
-
+#define led 2
+HTTPClient http;
 void postDataToServer(String macAddressLoc) {
- 
   Serial.println("Posting JSON data to server...");
-  // Block until we are able to connect to the WiFi access point
-  // HTTPClient http;   
-     
-  // http.begin("http://192.168.183.104:3001/device/move");  
-  // http.addHeader("Content-Type", "application/json");         
-     
   StaticJsonDocument<200> doc;
-  // Add values in the document
-  //
   doc["mac_address_router"] = WiFi.macAddress();
   doc["mac_address_moved"] = macAddressLoc;
-  // Add an array.
-  //
   JsonArray data = doc.createNestedArray("data");
-    
-     
   String requestBody;
   serializeJson(doc, requestBody);
-     
   int httpResponseCode = http.POST(requestBody);
- 
-    if(httpResponseCode>0){
-       
-      String response = http.getString();                       
-       
-      Serial.println(httpResponseCode);   
-      Serial.println(response);
-     
-    }
-     
+  if (httpResponseCode > 0) {
+    String response = http.getString();
+    Serial.println(httpResponseCode);
+    Serial.println(response);
+  }
 }
 
 void EnviarDados(String macAddLoc) {
-     Serial.println("Enviei o MACADRESS");
-     Serial.println(macAddLoc);
-    //DadosConexao();
-    postDataToServer(macAddLoc);       
+  Serial.println("Enviei o MACADRESS");
+  Serial.println(macAddLoc);
+  postDataToServer(macAddLoc);
 }
 
-
 void setup() {
+  WiFi.mode(WIFI_STA);
   Serial.begin(115200);
+  pinMode(led, OUTPUT);
+  WiFiManager wm;
+  bool res;
+  res = wm.autoConnect("Configurar-ESP32", "");
 
-  WiFi.begin(SSIDS,PWD);
-  while (WiFi.status() != WL_CONNECTED) {
-      Serial.print("Tentando novamente!");
-      delay(500);
+  if (!res) {
+    Serial.println("Failed to connect");
+    digitalWrite(led, HIGH);
+    delay(1000);
+    digitalWrite(led, LOW);
+    delay(1000);
+    digitalWrite(led, HIGH);
+    delay(1000);
+    digitalWrite(led, LOW);
+    delay(1000);
+    ESP.restart();
+  } else {
+    Serial.println("connected :)");
+    digitalWrite(led, HIGH);
+    http.begin("http://192.168.49.228:3001/device/move");
+    http.addHeader("Content-Type", "application/json");
   }
-
-  http.begin("http://192.168.183.104:3001/device/move");  
-  http.addHeader("Content-Type", "application/json");  
 }
 
 void loop() {
@@ -82,8 +71,6 @@ void loop() {
     Serial.print(n);
     Serial.println(" networks found");
     for (int i = 0; i < n; ++i) {
-      // Print SSID and RSSI for each network found
-      
       std::string ssid = (WiFi.SSID(i)).c_str();
       std::string ssidInitials = ssid.substr(0, 4);
 
@@ -104,8 +91,6 @@ void loop() {
       } 
     }
   }
-
-  // Wait a bit before scanning again
   delay(100);
 
 }
--- src/Back-end/ESP32/código-funcionando/tag.cpp
+++ src/Back-end/ESP32/código-funcionando/tag.cpp
@@ -1,18 +1,73 @@
+#include <WiFiManager.h>
 #include <Arduino.h>
 #include <WiFi.h>
 #include <iostream>
 #include <WiFiClient.h>
 #include "esp_wifi.h"
+#define led 2
 
-
-void setup() {
+void setup()
+{
+  WiFi.mode(WIFI_STA);
   Serial.begin(9600);
-  WiFi.mode(WIFI_AP_STA);
-  delay(100);
-  WiFi.softAP("Loc-1209", "$Inteli12345", 1);
-  delay(500);
+  pinMode(led, OUTPUT);
+  WiFiManager wm;
+  bool res;
+  res = wm.autoConnect("Configurar-ESP32", "");
+
+  if (!res)
+  {
+    Serial.println("Failed to connect");
+    digitalWrite(led, HIGH);
+    delay(1000);
+    digitalWrite(led, LOW);
+    delay(1000);
+    digitalWrite(led, HIGH);
+    delay(1000);
+    digitalWrite(led, LOW);
+    delay(1000);
+    ESP.restart();
+  }
+  else
+  {
+    Serial.println("connected :)");
+    digitalWrite(led, HIGH);
+    WiFi.softAP("Loc-1209", "$Inteli12345", 1);
+    delay(500);
+  }
 }
-void loop() {
+String getBuzzer()
+{
+  HTTPClient http;
+
+  http.begin("http://ip:3001/esp/buzzer/" + WiFi.macAddress());
+  http.addHeader("Content-Type", "application/json");
+
+  StaticJsonDocument<200> res;
+
+  int httpResponseCode = http.GET();
+
+  if (httpResponseCode > 0)
+  {
+
+    String response = http.getString();
+
+    Serial.println(httpResponseCode);
+    Serial.println(response);
+    deserializeJson(res, response);
+
+    String mac_adress = res["mac_address"];
+    return mac_adress;
+  }
+}
+void loop()
+{
   Serial.println("Deu certo");
   delay(10000);
+  String mac_addr = getBuzzer();
+
+  if (mac_addr = "1")
+  {
+    tone(48, 250, 500);
+  }
 }
